b2acfdb9bcc82662b45bd409b17a68614e827f07
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-05-18 22:23:50 +0200

    squashme: minor fix

--- unifr-evolutionary-genomics-practical1/init.d/install-gatk-picard.sh
+++ unifr-evolutionary-genomics-practical1/init.d/install-gatk-picard.sh
@@ -4,14 +4,13 @@ set -euo pipefail
 GATK_VERSION=4.6.2.0
 PICARD_VERSION=3.4.0
 PERSISTENT_DIR="${RENKU_MOUNT_DIR:-~}"
-GATK_INSTALL_DIR="${PERSISTENT_DIR}/gatk"
+GATK_INSTALL_DIR="${PERSISTENT_DIR}/gatk-${GATK_VERSION}"
 PICARD_INSTALL_DIR="${PERSISTENT_DIR}/picard"
 
 mkdir -p "$PICARD_INSTALL_DIR"
 mkdir -p "$GATK_INSTALL_DIR"
 curl -fsSL -o /tmp/gatk.zip https://github.com/broadinstitute/gatk/releases/download/${GATK_VERSION}/gatk-${GATK_VERSION}.zip
-unzip -q /tmp/gatk.zip -d "$GATK_INSTALL_DIR"
-cp -r "${GATK_INSTALL_DIR}/gatk-${GATK_VERSION}/*" "$GATK_INSTALL_DIR"
+unzip -q /tmp/gatk.zip -d "$PERSISTENT_DIR"
 rm -f /tmp/gatk.zip
 curl -fsSL -o "${PICARD_INSTALL_DIR}/picard.jar" https://github.com/broadinstitute/picard/releases/download/${PICARD_VERSION}/picard.jar
 cat > "${PICARD_INSTALL_DIR}/picard" << EOF
