d0eaf19cb894d3d563cbad7a7fb5c6501d2f5677
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-05-18 23:28:51 +0200

    chore: minor fix

--- unifr-evolutionary-genomics-practical1/init.d/install-gatk-picard.sh
+++ unifr-evolutionary-genomics-practical1/init.d/install-gatk-picard.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -euox pipefail
 
 GATK_VERSION=4.6.2.0
 PICARD_VERSION=3.4.0
-PERSISTENT_DIR="${RENKU_MOUNT_DIR:-~}"
+PERSISTENT_DIR="${RENKU_MOUNT_DIR:-$HOME}"
 GATK_INSTALL_DIR="${PERSISTENT_DIR}/gatk-${GATK_VERSION}"
 PICARD_INSTALL_DIR="${PERSISTENT_DIR}/picard"
 
@@ -12,18 +12,20 @@ curl -fsSL -o /tmp/gatk.zip https://github.com/broadinstitute/gatk/releases/down
 unzip -q /tmp/gatk.zip -d "$PERSISTENT_DIR"
 rm -f /tmp/gatk.zip
 curl -fsSL -o "${PICARD_INSTALL_DIR}/picard.jar" https://github.com/broadinstitute/picard/releases/download/${PICARD_VERSION}/picard.jar
-cat > "${PICARD_INSTALL_DIR}/picard" << EOF
+cat >"${PICARD_INSTALL_DIR}/picard" <<EOF
 #!/usr/bin/env bash
 exec java -jar ${PICARD_INSTALL_DIR}/picard.jar "\$@"
 EOF
 chmod +x "${PICARD_INSTALL_DIR}/picard"
 
-echo >> ~/.bashrc
-printf 'export PATH="%s:%s:$PATH"' "${GATK_INSTALL_DIR}" "${PICARD_INSTALL_DIR}" >> ~/.bashrc
-echo >> ~/.bashrc
+echo >>~/.bashrc
+printf 'export PATH="%s:%s:$PATH"' "${GATK_INSTALL_DIR}" "${PICARD_INSTALL_DIR}" >>~/.bashrc
+echo >>~/.bashrc
 
 PIPX_HOME="${PERSISTENT_DIR}/pipx"
 PIPX_BIN_DIR="${PERSISTENT_DIR}/pipx_bin"
 mkdir -p "$PIPX_HOME" "$PIPX_BIN_DIR"
-print 'export PATH="%s:$PATH"' "${PIPX_BIN_DIR}" >> ~/.bashrc
+printf 'export PATH="%s:$PATH"' "${PIPX_BIN_DIR}" >>~/.bashrc
+echo >>~/.bashrc
 pipx install multiqc==1.33
+
