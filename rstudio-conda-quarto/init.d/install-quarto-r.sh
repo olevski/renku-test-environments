aa6068ca1240db5a47ee5a6a54426079cf272fc4
Tasko Olevski <16360283+olevski@users.noreply.github.com>
2026-06-23 01:51:41 +0200

    minor fix

--- rstudio-conda-quarto/init.d/install-quarto-r.sh
+++ rstudio-conda-quarto/init.d/install-quarto-r.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 set -euox pipefail
-Rscript -e 'install.packages("quarto")'
+Rscript -e 'install.packages("quarto", repos="https://cloud.r-project.org")' || echo "Failed to install"
